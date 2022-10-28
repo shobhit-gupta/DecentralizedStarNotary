a74a2c6afe1631556dd58e2943a428b4bfd173ff
Shobhit Gupta <shobhitgupta@Shobhits-MacBook-Pro.local>
2022-10-28 11:56:28 +0530

    Feat: Add the ability to put up a star for sale

--- contracts/StarNotary.sol
+++ contracts/StarNotary.sol
@@ -14,10 +14,14 @@ contract StarNotary is ERC721 {
 
     // Associate the token with the Star.
     mapping(uint256 => Star) public tokenIdToStar;
+    // Associate the token with it's price.
+    mapping(uint256 => uint256) public starsForSale;
+
 
     // Call ERC721 constructor with relevant details.
     // Without this call this would be an abstract contract.
     constructor(string memory name_, string memory symbol_) ERC721(name_, symbol_) {}
+
 
     // Create Star using the Struct
     function createStar(string memory _name, uint256 _tokenId) public {
@@ -27,6 +31,14 @@ contract StarNotary is ERC721 {
         tokenIdToStar[_tokenId] = newStar;  
         // Assign msg.sender as the owner of the token
         _mint(msg.sender, _tokenId);    
+    }
+
+
+    // Put a Star for sale by adding the star tokenid into the mapping starsForSale.
+    // First verify that the sender is indeed the star's owner)
+    function putStarUpForSale(uint256 _tokenId, uint256 _price) public {
+        require(ownerOf(_tokenId) == msg.sender, "You can't sell the Star you don't own");
+        starsForSale[_tokenId] = _price;
     }
 
 }
--- test/TestStarNotary.js
+++ test/TestStarNotary.js
@@ -15,4 +15,17 @@ contract('StarNotary', accs => {
         const newStarName = await instance.tokenIdToStar.call(tokenId);
         assert.equal(newStarName, name, 'Names do not match');
     });
+
+
+    it('can put up a star for sale', async () => {
+        const instance = await StarNotary.deployed();
+        const tokenId = 2,
+              name = 'World Star',
+              user = accounts[1],
+              price = web3.utils.toWei('.01', 'ether');
+        await instance.createStar(name, tokenId, {from: user});
+        await instance.putStarUpForSale(tokenId, price, {from: user});
+        const starPrice = await instance.starsForSale.call(tokenId);
+        assert.equal(starPrice, price, 'Prices do not match');
+    });
 });
